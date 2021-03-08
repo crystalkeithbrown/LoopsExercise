8abf8afeef9cdb6ca491ad0b20b625565a7efe5c
Crystal Brown <crystalkeithbrown@gmail.com>
2021-03-08 10:10:53 -0500

    LoopsExercise Done

---
+++ loopsExercise.js
@@ -0,0 +1,30 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+for(i = 5; i <=25; i+=4){
+    console.log(i);
+}
+console.log ("========");
+
+const wizards = [
+    "Harry Potter", 
+    "Hermione Granger", 
+      "Ron Weasley"
+];
+for (name of wizards){
+    console.log(n);
+}
+let harryPotterMovies = 0;
+
+while (harryPotterMovies <=8){
+    harryPotterMovies++;
+}
+
+const hogwartsHouses ={
+    "Gryffindor", 
+    "Hufflepuff", 
+    "Ravenclaw", 
+    "Slytherin"
+    
+};
